4062d614fd75e30bdaf5680f997d984e1e109ec5
sssulai@gmail.com <you@example.com>
2025-09-08 18:57:59 -0400

    added photo gallery views

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
-import { Photos } from "./components/sections/Photos";
+//import { Gallery } from "./components/sections/Gallery.jsx";
 import { Contact } from "./components/sections/Contact";
 // Removed import App from "./App";
 
@@ -24,11 +24,11 @@ function App() {
         } bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+       {/* <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> */}
         <Home />
         <About />
         <Projects />
-        <Photos />
+        {/** <Gallery /> **/}
         <Contact />
         {/* Other sections like About, Projects, Contact would go here */}
       </div>
