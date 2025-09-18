b28802be95b1a0c153fc7356e35db6adc60ffbc1
sssulai@gmail.com <you@example.com>
2025-09-18 19:36:54 -0400

    worked on contact page

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,4 +1,3 @@
-// src/components/sections/Contact.jsx
 import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
 import emailjs from "emailjs-com";
@@ -10,22 +9,21 @@ export const Contact = () => {
     message: "",
   });
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(
-        import.meta.env.VITE_SERVICE_ID,
-        import.meta.env.VITE_TEMPLATE_ID,
-        e.target,
-        import.meta.env.VITE_PUBLIC_KEY
-      )
+      .sendForm(import.meta.env.VITE_SERVICE_ID,
+       import.meta.env.VITE_TEMPLATE_ID, e.target,
+       import.meta.env.VITE_PUBLIC_KEY)
       .then((result) => {
         alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
       })
       .catch(() => alert("Oops! Something went wrong. Please try again."));
   };
+
 
   return (
     <section
@@ -35,6 +33,7 @@ export const Contact = () => {
       <RevealOnScroll>
         <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
             Get In Touch
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
@@ -52,6 +51,7 @@ export const Contact = () => {
                 }
               />
             </div>
+
             <div className="relative">
               <input
                 type="email"
@@ -66,6 +66,7 @@ export const Contact = () => {
                 }
               />
             </div>
+
             <div className="relative">
               <textarea
                 id="message"
@@ -80,6 +81,7 @@ export const Contact = () => {
                 }
               />
             </div>
+
             <button
               type="submit"
               className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
