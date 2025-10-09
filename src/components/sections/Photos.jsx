b5cb98342e057822c6abdbd801aeb604a2ae5e34
sssulai@gmail.com <you@example.com>
2025-10-09 19:07:35 -0400

    styled  photo view

--- src/components/sections/Photos.jsx
+++ src/components/sections/Photos.jsx
@@ -1,15 +1,24 @@
-
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Photos = () => {
   return (
     <section id="photos" className="py-24 px-4 relative">
       {""}
-      <div className="bg-white rounded-lg overflow-hidden">
-        <div className="container mx-auto max-w-5xl">
-          <h2>Photos</h2>
-          <img class="h-48 w-96 object-cover ..." src="/img/mountains.jpg" />
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-600 to-[#71D7F5] bg-clip-text text-transparent text-center">
+            Photos
+          </h2>
+          <div className="rounded-xl p-8 border border-white/10 hover:-translate-y-1 transition-all">
+            <div className="container mx-auto max-w-5xl">
+              <img
+                class="h-48 w-96 object-cover ..."
+                src="public/horsecloud.JPG"
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
-}
+};
